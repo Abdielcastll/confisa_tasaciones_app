f5a92bf30061ee4d50d76d309e49b1120909ffc9
anibalvc <anibalvc1999@gmail.com>
2022-07-13 14:05:24 -0400

    Media implementación actualizar permiso

--- lib/views/entidades_seguridad/widgets/table_permisos_widget.dart
+++ lib/views/entidades_seguridad/widgets/table_permisos_widget.dart
@@ -1,12 +1,18 @@
 import 'package:advanced_datatable/advanced_datatable_source.dart';
 import 'package:advanced_datatable/datatable.dart';
 import 'package:flutter/material.dart';
+import 'package:tasaciones_app/views/entidades_seguridad/widgets/form_crear_permiso.dart';
 
+import '../../../core/api/acciones_api.dart';
 import '../../../core/api/api_status.dart';
 import '../../../core/api/permisos_api.dart';
+import '../../../core/api/recursos_api.dart';
 import '../../../core/authentication_client.dart';
 import '../../../core/locator.dart';
+import '../../../core/models/acciones_response.dart';
 import '../../../core/models/permisos_response.dart';
+import '../../../core/models/recursos_response.dart';
+import '../../../widgets/app_circle_icon_button.dart';
 import '../../../widgets/app_dialogs.dart';
 
 class PaginatedTablePermisos {
@@ -38,7 +44,9 @@ class TablePermisos extends AdvancedDataTableSource<PermisosData> {
   TablePermisos({required this.pageSize, required this.context});
   late BuildContext context;
   final user = locator<AuthenticationClient>().loadSession;
-  final _permisosAPI = locator<PermisosAPI>();
+  final _accionesApi = locator<AccionesAPI>();
+  final _recursosApi = locator<RecursosAPI>();
+  final _permisosApi = locator<PermisosAPI>();
   @override
   bool get forceRemoteReload => super.forceRemoteReload = true;
   int pageSize;
@@ -55,12 +63,75 @@ class TablePermisos extends AdvancedDataTableSource<PermisosData> {
         Text(currentRowData.descripcion),
       ),
       DataCell(IconButton(
-          onPressed: () {
-            currentPage = 0;
-            offset = 0;
-            totalCount = 0;
-            data = [];
-            setNextView();
+          onPressed: () async {
+            GlobalKey<FormState> _key = GlobalKey();
+            String descripcion = currentRowData.descripcion;
+            Map<String, dynamic> accion = {};
+            Map<String, dynamic> recurso = {};
+            dynamic opcion;
+            ProgressDialog.show(context);
+            var resp = await _accionesApi.getAcciones();
+            if (resp is Success<AccionesResponse>) {
+              var resp2 = await _recursosApi.getRecursos();
+              if (resp2 is Success<RecursosResponse>) {
+                ProgressDialog.dissmiss(context);
+                final GlobalKey<FormState> _formKey = GlobalKey();
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                          clipBehavior: Clip.none,
+                          content: formCrearPermiso(
+                            _formKey,
+                            descripcion,
+                            recurso,
+                            accion,
+                            (String descripcionf) async {
+                              ProgressDialog.show(context);
+                              var creacion = await _permisosApi.createPermisos(
+                                  descripcion: descripcionf,
+                                  idAccion: accion["id"],
+                                  idRecurso: recurso["id"],
+                                  esBasico: 1);
+                              if (creacion is Success<PermisosData>) {
+                                ProgressDialog.dissmiss(context);
+                                Dialogs.alert(context,
+                                    tittle: "Modificacion exitosa",
+                                    description: [
+                                      "Permiso modificado con exito"
+                                    ]);
+                                _formKey.currentState?.reset();
+                              } else if (creacion is Failure) {
+                                ProgressDialog.dissmiss(context);
+                                Dialogs.alert(context,
+                                    tittle: creacion.supportMessage,
+                                    description: creacion.messages);
+                              }
+                            },
+                            opcion,
+                            resp.response.data,
+                            resp2.response.data,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ));
+                    });
+              } else if (resp2 is Failure) {
+                ProgressDialog.dissmiss(context);
+                Dialogs.alert(
+                  context,
+                  tittle: 'Error',
+                  description: resp2.messages,
+                );
+              }
+            } else if (resp is Failure) {
+              ProgressDialog.dissmiss(context);
+              Dialogs.alert(
+                context,
+                tittle: 'Error',
+                description: resp.messages,
+              );
+            }
           },
           icon: const Icon(Icons.cached))),
       DataCell(IconButton(
@@ -71,7 +142,7 @@ class TablePermisos extends AdvancedDataTableSource<PermisosData> {
                 confirm: () async {
               ProgressDialog.show(context);
               var resp =
-                  await _permisosAPI.deletePermisos(id: currentRowData.id);
+                  await _permisosApi.deletePermisos(id: currentRowData.id);
               if (resp is Success<PermisosPOSTResponse>) {
                 ProgressDialog.dissmiss(context);
                 Dialogs.alert(context,
@@ -111,7 +182,7 @@ class TablePermisos extends AdvancedDataTableSource<PermisosData> {
       offset = pageRequest.offset;
       return RemoteDataSourceDetails(totalCount, dataList);
     } else {
-      var resp = await _permisosAPI.getPermisos(
+      var resp = await _permisosApi.getPermisos(
           pageNumber: currentPage + 1, pageSize: pageRequest.pageSize);
       if (resp is Success<PermisosResponse>) {
         for (var element in resp.response.data) {
